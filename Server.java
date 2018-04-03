93cd5581a3fbfe48d50a70e5427da9d634ed1b12
ZahraShayesteh <37510195+ZahraShayesteh@users.noreply.github.com>
2018-04-04 00:37:33 +0430

    update of server with less lines & comments added

--- Server.java
+++ Server.java
@@ -1,4 +1,4 @@
-package HW11_simpleChat;
+package fgh;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
@@ -8,8 +8,8 @@ import java.net.Socket;
 import java.net.UnknownHostException;
 
 public class Server {
-	static String line1 = "";
-	static String line2 = "";
+	static String line1 = "";//input stream of client1
+	static String line2 = "";//input stream of client2
 
 	public static void main(String[] args) throws UnknownHostException, IOException {
 		// TODO Auto-generated method stub
@@ -21,38 +21,32 @@ public class Server {
 		DataInputStream is2 = new DataInputStream(client2.getInputStream());
 		DataOutputStream os2 = new DataOutputStream(client2.getOutputStream());
 
-		class readFromSocketClient1Thread extends Thread {
+		class readFromSocketClient1Thread extends Thread {//observing client1
 			public void run() {
 				while (!line1.equals("q") & !line2.equals("q")) {
 					try {
-						line1 = is1.readLine();
-						try {
-							if (line1.equals("q") || line2.equals("q")) {
-								os2.writeBytes(line1 + "\n");
-								System.exit(0);
-							}
-							os2.writeBytes(line1 + "\n");
-						} catch (IOException e) {
+						line1 = is1.readLine();//reading input stream of socket client 1
+						if (line1.equals("q") || line2.equals("q")) {
+							os2.writeBytes(line1 + "\n");//writing line1 on socket client2
+							System.exit(0);//chat will terminate
 						}
+						os2.writeBytes(line1 + "\n");//writing line1 on socket client2
 					} catch (IOException e) {
 					}
 				}
 			}
 		}
 
-		class readFromSocketClient2Thread extends Thread {
+		class readFromSocketClient2Thread extends Thread {//observing client2
 			public void run() {
 				while (!line1.equals("q") & !line2.equals("q")) {
 					try {
-						line2 = is2.readLine();
-						try {
-							if (line1.equals("q") || line2.equals("q")) {
-								os1.writeBytes(line2 + "\n");
-								System.exit(0);
-							}
-							os1.writeBytes(line2 + "\n");
-						} catch (IOException e) {
+						line2 = is2.readLine();//reading input stream of socket client 2
+						if (line1.equals("q") || line2.equals("q")) {
+							os1.writeBytes(line2 + "\n");//writing line2 on socket client1
+							System.exit(0);//chat will terminate
 						}
+						os1.writeBytes(line2 + "\n");//writing line2 on socket client1
 					} catch (IOException e) {
 					}
 				}
@@ -60,9 +54,9 @@ public class Server {
 		}
 		readFromSocketClient1Thread tread1 = new readFromSocketClient1Thread();
 		readFromSocketClient2Thread tread2 = new readFromSocketClient2Thread();
-		tread1.start();
-		tread2.start();
-		if (line1.equals("q") || line2.equals("q")) {
+		tread1.start();//starting the thread of observing client1
+		tread2.start();//starting the thread of observing client2
+		if (line1.equals("q") || line2.equals("q")) {//if input streams of sockets are "q" socket and chat will terminated
 			server.close();
 			client1.close();
 			client2.close();
